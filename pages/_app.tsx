cfbf99220a3e1a37fe0800df9b312438268a41c4
Ludwig <ludwig.slettingdalen@gmail.com>
2022-09-12 14:35:06 +0200

    Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
-import { QueryClientProvider, QueryClient } from "react-query"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
 
 const queryClient = new QueryClient
 

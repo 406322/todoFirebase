ab8b8da55f0a876666c4cde099df67c45c9e3105
Ludwig <ludwig.slettingdalen@gmail.com>
2022-09-13 21:40:56 +0200

    Changed Firebase Project to todo2

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxVdikPvtrpuwr4uxc90Jsf8a74x06cg0",
-    authDomain: "fir-df0a9.firebaseapp.com",
-    projectId: "fir-df0a9",
-    storageBucket: "fir-df0a9.appspot.com",
-    messagingSenderId: "289664701284",
-    appId: "1:289664701284:web:e922d47fdfd9e60a7d2071"
+    apiKey: "AIzaSyD9enElehAflMUHeQcqHIZGsccLeTOdyTQ",
+    authDomain: "todo2-9d3ff.firebaseapp.com",
+    projectId: "todo2-9d3ff",
+    storageBucket: "todo2-9d3ff.appspot.com",
+    messagingSenderId: "99658098602",
+    appId: "1:99658098602:web:686543278ea291482aadd3"
 };
 
 export const firebase = initializeApp(firebaseConfig)

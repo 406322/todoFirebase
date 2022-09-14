471aef371936238da9752608292165184b53aeb4
Ludwig <ludwig.slettingdalen@gmail.com>
2022-09-14 12:01:41 +0200

    PUT toggleComplete working

--- components/TodoListItem.tsx
+++ components/TodoListItem.tsx
@@ -1,11 +1,10 @@
 import { Todo } from "../models/todo";
 import { TiDeleteOutline } from 'react-icons/ti';
-import { GrEdit } from 'react-icons/gr';
 import { BiSave } from 'react-icons/bi';
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import axios from "axios";
 import { BiPencil } from "react-icons/bi";
-import { doc, deleteDoc } from "@firebase/firestore";
+import { doc, deleteDoc, updateDoc } from "@firebase/firestore";
 import { db } from "../firebase/firebase";
 
 
@@ -47,10 +46,18 @@ export const TodoListItem = ({ todo, todos, setTodos }: { todo: Todo, todos: Tod
         deleteDoc(docRef)
     }
 
-    const handleEdit = () => {
-        toggleEditToSaved()
-        toggleEdit();
-    };
+    const toggleComplete = () => {
+        const docRef = doc(db, 'TodoList', todo.id)
+        updateDoc(docRef, {
+            isComplete: !todo.isComplete
+        })
+    }
+
+
+    // const handleEdit = () => {
+    //     toggleEditToSaved()
+    //     toggleEdit();
+    // };
 
     const handleSave = async () => {
         setTodos(
@@ -67,21 +74,6 @@ export const TodoListItem = ({ todo, todos, setTodos }: { todo: Todo, todos: Tod
             })
         );
     }
-
-    const handleToggleComplete = (): void => {
-        setTodos(
-            todos.map((element: Todo) => {
-                if (element.id === todo.id) {
-                    const tempTodo = { ...element };
-                    tempTodo.isComplete = !tempTodo.isComplete;
-                    const data = { isComplete: tempTodo.isComplete, id: tempTodo.id }
-                    const response = axios.put("/api", data);
-                    return tempTodo;
-                }
-                return element;
-            })
-        );
-    };
 
     const toggleEdit = () => {
         setTodos(
@@ -113,7 +105,7 @@ export const TodoListItem = ({ todo, todos, setTodos }: { todo: Todo, todos: Tod
     return (
         <>
             <form
-                onSubmit={handleSave}
+                onSubmit={blur}
                 className="flex justify-between gap-3 p-5 m-5 bg-[#201c1b] rounded-md ">
 
                 <div className="flex gap-3">
@@ -121,7 +113,7 @@ export const TodoListItem = ({ todo, todos, setTodos }: { todo: Todo, todos: Tod
                         type="checkbox"
                         checked={todo.isComplete}
                         className="rounded-full cursor-pointer w-7 h-7"
-                        onChange={handleToggleComplete}
+                        onChange={toggleComplete}
                     />
 
                     < input
@@ -147,13 +139,13 @@ export const TodoListItem = ({ todo, todos, setTodos }: { todo: Todo, todos: Tod
                         ? <BiSave
                             title="Save Todo"
                             className="text-white cursor-pointer w-7 h-7"
-                            onClick={handleSave}
+                            onClick={blur}
                         />
 
                         : <BiPencil
                             title="Edit Todo"
                             className="text-white cursor-pointer w-7 h-7"
-                            onClick={handleEdit}
+                            onClick={focus}
                         />
                     }
                 </div>

03151bdf7743d3ea9120f738fbe2cf86f4b579d0
Ludwig <ludwig.slettingdalen@gmail.com>
2022-09-17 18:34:18 +0200

    Added sort by isComplete

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -7,7 +7,9 @@ export const TodoList = ({ todos }: { todos: any }) => {
     return (
         <div className="min-h-screen bg-[#201c1b] divide-cyan-100 px-10">
             {todos &&
-                todos.map((todo: Todo) => <TodoListItem key={todo.id} todo={todo} todos={todos} />)}
+                todos
+                    .sort((a: Todo, b: Todo) => Number(a.isComplete) - Number(b.isComplete))
+                    .map((todo: Todo) => <TodoListItem key={todo.id} todo={todo} todos={todos} />)}
         </div>
 
     );

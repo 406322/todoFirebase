bbb71cfb62b4c469de111c300484e44e88587fad
Ludwig <ludwig.slettingdalen@gmail.com>
2022-10-24 21:23:31 +0200

    changed fill-rule to fillRule

--- components/icons.tsx
+++ components/icons.tsx
@@ -13,7 +13,7 @@ export const LogoutIcon = ({ className: className }: { className: any }) => {
 
 export const PlusIcon = ({ className: className }: { className: any }) => {
     return (
-        <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 12 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12 9H7v5H5V9H0V7h5V2h2v5h5v2z"></path></svg>)
+        <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 12 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12 9H7v5H5V9H0V7h5V2h2v5h5v2z"></path></svg>)
 }
 
 export const CarretDownIcon = ({ className: className }: { className: any }) => {
